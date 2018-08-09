5c1d8aaea8f65a01d2bc0bb2e1f5ed67f849a319
Nicole Li <nicolehcli@gmail.com>
2018-08-08 20:03:14 -0700

    Added design and randomisation for dice roll

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    let diceArray2 = [#imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex1)
+        print(randomDiceIndex2)
+        
+        // way 1 - UIImage(named: String)
+        var dice1String = "dice" + String(randomDiceIndex1 + 1)
+        diceImageView1.image = UIImage(named: dice1String)
+        
+        // way 2-  directly using image
+        diceImageView2.image = diceArray2[randomDiceIndex2]
+    }
+    
 }
 
